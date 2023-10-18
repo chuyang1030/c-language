250167311c4d00eb7653285d5a97757be8264132
初阳1 <3031330024@qq.com>
2023-10-18 23:50:37 +0800

    23.10.18

--- 学生基本信息输入输出.c
+++ 学生基本信息输入输出.c
@@ -1,16 +1,16 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-
 int main()
 {
-    int a = 0;
-    float b = 0;
-    float c = 0;
-    float d = 0;
+    char a;
 
-    scanf("%d;%f,%f,%f", &a, &b, &c, &d);
-
-    printf("The each subject score of No. %d is %.2f, %.2f, %.2f.", a, b, c, d);
+    scanf("%c", &a);
+    printf("     %c\n", a);
+    printf("    %c %c\n", a);
+    printf("   %c %c %c\n", a);
+    printf("  %c %c %c %c\n", a);
+    printf(" %c %c %c %c %c\n", a);
 
     return 0;
+
 }
