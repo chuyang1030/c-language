ee04116e495c08441231e462a32792f86187ba2a
初阳1 <3031330024@qq.com>
2023-10-31 01:55:57 +0000

    update 不借助中间值交换两数.c.
    Signed-off-by: 初阳1 <3031330024@qq.com>

--- 不借助中间值交换两数.c
+++ 不借助中间值交换两数.c
@@ -1,5 +1,5 @@
 #define  _CRT_SECURE_NO_WARNINGS 1
-#include <stdio.h>
+方法一：#include <stdio.h>
 int main()
 {
 	int a, b;
@@ -7,6 +7,18 @@ int main()
 	a = a + b;
 	b = a - b;
 	a = a - b;
+	printf("%d %d", a, b);
+	return 0;
+}
+方法二：#include <stdio.h>
+//异或^相同为零 相异为一 异或支持交换
+int main()
+{
+	int a, b;
+	scanf("%d %d", &a, &b);
+	a = a ^ b;//a^b
+	b = a ^ b;//a^b^b=a
+	a = a ^ b;//a^b^a=b
 	printf("%d %d", a, b);
 	return 0;
 }
